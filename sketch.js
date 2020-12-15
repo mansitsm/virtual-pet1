bbc9af7871504921a4dfb9b105cfaacc3abd2558
mansitsm <70889115+mansitsm@users.noreply.github.com>
2020-12-15 20:00:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+//Create variables here
+var dog,happydog,database,foodS,foodStock; 
+function preload()
+{
+  //load images here
+  dog=loadImage("dogImg.png");
+}
+
+function setup() {
+	createCanvas(500, 500);
+  dog=createSprite(250,250,20,20);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
